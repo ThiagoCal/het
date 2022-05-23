1d68ecd09a772ccfc2f6177ac612387a55888e6f
Thiago Calazans <thiagotorrescalazans@gmail.com>
2022-05-23 01:26:11 -0300

    changed way to copy pix key

--- components/Pix.js
+++ components/Pix.js
@@ -3,6 +3,7 @@ import banner from "../public/bannerwed.jpg";
 import { useState, useRef } from 'react';
 import ReactDOM from 'react-dom';
 import qrcode from '../public/qrcodecasamento.jpeg';
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 function Pix() {
   const [copySuccess, setCopySuccess] = useState('');
@@ -32,17 +33,22 @@ function Pix() {
           </Image>
         </div>
         <div>
-            <div>
+            {/* <div>
               <button className="btn btn-success" onClick={copyToClipboard}>Copy</button>
               {copySuccess}
-            </div>
+            </div> */}
           <form>
             <div className="mt-3">
-              <textarea
+            <CopyToClipboard
+              text="00020126650014br.gov.bcb.pix0130thiagotorrescalazans@gmail.com0209Casamento5204000053039865802BR5925THIAGO ROSSITER GUIMARAES6014RIO DE JANEIRO62250521Casamentohannaethiago6304C020"
+              onCopy={() => alert("Copiada")}>
+                <button className="btn btn-success">Copie a chave Pix</button>
+            </CopyToClipboard>
+              {/* <textarea
                 ref={textAreaRef}
                 // disabled={true}
                 defaultValue='00020126650014br.gov.bcb.pix0130thiagotorrescalazans@gmail.com0209Casamento5204000053039865802BR5925THIAGO ROSSITER GUIMARAES6014RIO DE JANEIRO62250521Casamentohannaethiago6304C020'
-              />
+              /> */}
             </div>
           </form>
         </div>
